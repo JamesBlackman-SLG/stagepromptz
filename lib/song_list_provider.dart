f9f5c14c6dfb0e6271ee738134c394ccd8362463
JamesBlackman-SLG <83969784+JamesBlackman-SLG@users.noreply.github.com>
2024-04-29 00:07:40 +0100

    Tidy

--- lib/song_list_provider.dart
+++ lib/song_list_provider.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' show ChangeNotifier;
+
 import 'song_model.dart';
 
 class SongListProvider with ChangeNotifier {
@@ -18,16 +19,16 @@ class SongListProvider with ChangeNotifier {
         lyrics: "In the town where I was born..."),
   ];
 
-  List<Song> get songs => _songs;
   int _currentIndex = 0;
+  Song? _editingSong;
 
+  int _nextId = 5;
   int get currentIndex => _currentIndex;
+
   set currentIndex(int index) {
     _currentIndex = index;
     notifyListeners();
   }
-
-  Song? _editingSong;
 
   Song? get editingSong => _editingSong;
   set editingSong(Song? song) {
@@ -35,11 +36,12 @@ class SongListProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  int _nextId = 5;
   int get nextId {
     _nextId++;
     return _nextId;
   }
+
+  List<Song> get songs => _songs;
 
   void addSong(Song song) {
     if (_songs.where((element) => element.id == song.id).isEmpty) {
@@ -50,21 +52,6 @@ class SongListProvider with ChangeNotifier {
     final index = _songs.indexWhere((element) => element.id == song.id);
     _songs[index] = song;
     notifyListeners();
-  }
-
-  void removeSong(int index) {
-    if (index >= 0 && index < _songs.length) {
-      _songs.removeAt(index);
-      notifyListeners();
-    }
-  }
-
-  void updateSong(Song song) {
-    final index = _songs.indexWhere((element) => element.id == song.id);
-    if (index >= 0) {
-      _songs[index] = song;
-      notifyListeners();
-    }
   }
 
   void cutSong(int index) {
@@ -79,6 +66,21 @@ class SongListProvider with ChangeNotifier {
     if (_editingSong != null) {
       _songs.insert(_currentIndex, _editingSong!);
       _editingSong = null;
+      notifyListeners();
+    }
+  }
+
+  void removeSong(int index) {
+    if (index >= 0 && index < _songs.length) {
+      _songs.removeAt(index);
+      notifyListeners();
+    }
+  }
+
+  void updateSong(Song song) {
+    final index = _songs.indexWhere((element) => element.id == song.id);
+    if (index >= 0) {
+      _songs[index] = song;
       notifyListeners();
     }
   }
