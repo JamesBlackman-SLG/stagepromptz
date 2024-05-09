63dcc3e75fc466c8041cba9fba7a5d3d30ae91ce
JamesBlackman-SLG <83969784+JamesBlackman-SLG@users.noreply.github.com>
2024-05-09 10:01:59 +0100

    added household items list

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:stagepromptz/keyboard_shortcut.dart';
-import 'action_intents.dart';
 import 'settings_provider.dart';
 import 'song_list_provider.dart';
 import 'song_list.dart';
