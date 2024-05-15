69f680583eccc19d8e9550b2ae6016ee070200a7
JamesBlackman-SLG <83969784+JamesBlackman-SLG@users.noreply.github.com>
2024-05-15 17:22:53 +0100

    Update .nvim.lua

--- .nvim.lua
+++ .nvim.lua
@@ -2,11 +2,11 @@
 -- If you have more than one setup configured you will be prompted when you run
 -- your app to select which one you want to use
 require("flutter-tools").setup_project({
-	-- {
-	-- 	name = "windows",
-	-- 	device = "windows",
-	-- 	target = "lib/main.dart",
-	-- },
+	{
+		name = "windows",
+		device = "windows",
+		target = "lib/main.dart",
+	},
 	{
 		name = "android",
 		device = "R52R10BLV4L",
